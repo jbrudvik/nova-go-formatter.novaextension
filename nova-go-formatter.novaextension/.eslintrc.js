e334f41879bcf4011ba10815eb9bf0fc9ebcd7ca
Jeremy Brudvik <jeremy@brudvik.me>
2022-05-26 14:08:55 -0400

    Initial working version

---
+++ nova-go-formatter.novaextension/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  globals: {
+    nova: "readonly",
+    Process: "readonly",
+    NotificationRequest: "readonly",
+  },
+  extends: "airbnb",
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    quotes: ["error", "double"],
+    "no-console": "off",
+  },
+};
---
+++ nova-go-formatter.novaextension/Scripts/main.js
@@ -0,0 +1,77 @@
+function displayError(message) {
+  // Log error to console
+  console.error(message);
+
+  // Display error in notification to user
+  const request = new NotificationRequest();
+  request.body = message;
+  nova.notifications.add(request).catch((err) => console.error(err, err.stack));
+}
+
+async function writeDocument(editor, content) {
+  const rangeEntireDocument = new Range(0, editor.document.length);
+  await editor.edit((edit) => {
+    // Only write if there are changes
+    const originalText = editor.getTextInRange(rangeEntireDocument);
+    if (originalText !== content) {
+      edit.replace(rangeEntireDocument, content);
+    }
+  });
+}
+
+async function formatGoFile(editor) {
+  let resolve;
+  let reject;
+  const promise = new Promise((_resolve, _reject) => {
+    resolve = _resolve;
+    reject = _reject;
+  });
+
+  const options = {
+    args: ["gofmt", editor.document.path],
+  };
+  const operationTitle = "Format Go file";
+  const command = `$ ${options.args.join(" ")}`;
+  const commandMessage = `${operationTitle}: ${command}`;
+
+  console.log(commandMessage);
+
+  const process = new Process("/usr/bin/env", options);
+
+  let formattedCode = "";
+  let errorMessage = `Error when attempting to format Go file:\n\n${command}`;
+
+  process.onStdout((line) => {
+    formattedCode += line;
+  });
+
+  process.onStderr((error) => {
+    errorMessage += `\n\n${error}`;
+  });
+
+  process.onDidExit((exitCode) => {
+    if (exitCode === 0) {
+      const successMessage = `${operationTitle}: Success`;
+      console.log(successMessage);
+      writeDocument(editor, formattedCode);
+      resolve(null);
+    } else {
+      displayError(errorMessage);
+      reject(errorMessage);
+    }
+  });
+
+  process.start();
+
+  return promise;
+}
+
+exports.activate = async () => {
+  nova.commands.register("go-formatter.goFormat", formatGoFile);
+
+  nova.workspace.onDidAddTextEditor((editor) => {
+    editor.onWillSave(formatGoFile);
+  });
+};
+
+exports.deactivate = () => {};
